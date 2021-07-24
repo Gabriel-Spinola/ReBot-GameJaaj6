39e0aa1d3653e21857dcdfb1f3affb8f66c53831
gseven-gdv <zerogravitystd@gmail.com>
2021-07-24 15:23:04 -0300

    Update RedHog.cs

--- Ciclos/Assets/Scritps/Enemies/RedHog.cs
+++ Ciclos/Assets/Scritps/Enemies/RedHog.cs
@@ -7,30 +7,43 @@ public class RedHog : EnemyPatrol
     [Header("RedHog References")]
     [SerializeField] private GameObject bullet;
 
-    [Header("Redhog Stats")]
+    [Header("RefHog Stats")]
+    [SerializeField] private float friction = .2f;
     [Range(1f, 5f)]
     [SerializeField] private float attackRange = 1f;
+    [Tooltip("In Seconds")]
     [SerializeField] private float waitToAttack = 1.5f;
-    [SerializeField] private float fireRate = 1f;
-    [SerializeField] private float damage = 1f;
-    [SerializeField] private float bulletSpeed = 8f;
-    [SerializeField] private int shootAngles = 5;
 
+    [Header("Redhog Bullet Config")]
+    [SerializeField] private float bulletSpeed = 8f;
+    [SerializeField] private float damage = 1f;
+
+    [Header("RedHog Shooting")]
+    [SerializeField] private float fireRate = 1f;
+    [SerializeField] private int amountOfBulletsPerShoot = 5;
+    [SerializeField] private float initialAngle = 25f;
+    [SerializeField] private float angle = 25f;
+    [SerializeField] private float maxRotation = 70f;
+    
     private float nextTimeToFire = 0f;
-    private int angles = 0;
+    private float currentShootAngle = 0;
+
+    private bool isAttacking = false;
 
     private void Start()
     {
-        
+        currentShootAngle = initialAngle;
     }
 
     private new void Update()
     {
-        base.Update();
+        if (!isAttacking)
+            base.Update();
 
-        if (Physics2D.OverlapCircle(transform.position, attackRange, whatIsPlayer)) {
-            mustPatrol = false;
-            walkSpeed = 0f;
+        isAttacking = Physics2D.OverlapCircle(transform.position, attackRange, whatIsPlayer);
+
+        if (isAttacking) {
+            rb.velocity = Vector2.right * Mathf.Lerp(rb.velocity.x, 0f, friction) + Vector2.up * rb.velocity.y;
 
             StartCoroutine(Attack(waitToAttack));
         }
@@ -43,17 +56,21 @@ public class RedHog : EnemyPatrol
         if (Time.time >= nextTimeToFire) {
             nextTimeToFire = Time.time + 1f / fireRate;
 
-            for (int i = 1; i <= shootAngles; i++) {
-                angles += i * 25;
+            for (int i = 1; i <= amountOfBulletsPerShoot; i++) {
+                if (currentShootAngle >= amountOfBulletsPerShoot * angle || currentShootAngle >= maxRotation) {
+                    currentShootAngle = initialAngle;
+                }
+
+                currentShootAngle += i * angle;
+
+                CommonBullet bullet_ = Instantiate(bullet, transform.position, transform.rotation).GetComponent<CommonBullet>();
+
+                bullet_.damage = damage;
+                bullet_.speed = bulletSpeed;
+                bullet_.dir = bullet_.transform.right;
+                bullet_.xScale = bullet_.transform.localScale.x;
+                bullet_.transform.rotation = Quaternion.Euler(bullet_.transform.rotation.x, bullet_.transform.rotation.y, currentShootAngle);
             }
-
-            CommonBullet bullet_ = Instantiate(bullet, transform.position, transform.rotation).GetComponent<CommonBullet>();
-
-            bullet_.damage = damage;
-            bullet_.speed = bulletSpeed;
-            bullet_.dir = bullet_.transform.right;
-            bullet_.xScale = bullet_.transform.localScale.x;
-            bullet_.transform.rotation = Quaternion.Euler(bullet_.transform.rotation.x, bullet_.transform.rotation.y, angles);
         }
 
         yield return 0;
