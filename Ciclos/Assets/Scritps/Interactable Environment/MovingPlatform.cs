e1bdeba1cdb4c8defac0f4ec765ba48168277b63
gseven-gdv <zerogravitystd@gmail.com>
2021-07-24 18:39:52 -0300

    Moving platform update

--- Ciclos/Assets/Scritps/Interactable Environment/MovingPlatform.cs
+++ Ciclos/Assets/Scritps/Interactable Environment/MovingPlatform.cs
@@ -9,10 +9,13 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private Transform startPos;
 
     [SerializeField] private float speed;
+    [SerializeField] private float delayToMove;
 
     private Rigidbody2D rb;
 
     private Vector3 nextPos;
+
+    private bool canMove = true;
 
     private void Awake()
     {
@@ -26,15 +29,31 @@ public class MovingPlatform : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (!canMove)
+            return;
+
         if (transform.position == pos1.position) {
             nextPos = pos2.position;
+
+            StartCoroutine(WaitToMove(delayToMove));
         }
 
         if (transform.position == pos2.position) {
             nextPos = pos1.position;
+
+            StartCoroutine(WaitToMove(delayToMove));
         }
 
         rb.transform.position = Vector2.MoveTowards(transform.position, nextPos, speed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator WaitToMove(float time)
+    {
+        canMove = false;
+
+        yield return new WaitForSeconds(time);
+
+        canMove = true;
     }
 
     private void OnDrawGizmosSelected()
