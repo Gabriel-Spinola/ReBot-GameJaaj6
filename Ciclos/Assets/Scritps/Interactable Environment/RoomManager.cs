d2c59b18606299099bc149dc013bab43e2e65d3d
gseven-gdv <zerogravitystd@gmail.com>
2021-07-25 18:54:44 -0300

    RoomManager

---
+++ Ciclos/Assets/Scritps/Interactable Environment/RoomManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    private GameObject virtualCamera;
+
+    private void Awake()
+    {
+        virtualCamera = transform.GetChild(0).gameObject;
+        virtualCamera.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(true);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
