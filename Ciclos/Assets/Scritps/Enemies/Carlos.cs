b1025aa072ca3d80bec01bcdaab1360c401c46ad
gseven-gdv <zerogravitystd@gmail.com>
2021-07-28 20:15:58 -0300

    aim

--- Ciclos/Assets/Scritps/Enemies/Carlos.cs
+++ Ciclos/Assets/Scritps/Enemies/Carlos.cs
@@ -6,16 +6,33 @@ public class Carlos : Enemy
 {
     [Header("Carlos References")]
     [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform shootPoint;
 
     [Header("Carlos Stats")]
+    [SerializeField] private bool shouldAim = false;
     [SerializeField] private float fireRate = 1f;
     [SerializeField] private float damage = 1f;
     [SerializeField] private float bulletSpeed = 2f;
 
+    private float lookDir = 0f;
+
     private float nextTimeToFire = 0f;
+
+    private void Awake()
+    {
+        if (shouldAim) {
+
+        }
+    }
 
     private void Update()
     {
+        if (shouldAim) {
+            lookDir = StaticRes.LookDir(transform.position, player.position);
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, lookDir));
+        }
+
         if (Time.time >= nextTimeToFire) {
             nextTimeToFire = Time.time + 1f / fireRate;
 
@@ -25,7 +42,7 @@ public class Carlos : Enemy
 
     private void Shoot()
     {
-        CommonBullet currentBullet = Instantiate(bullet, transform.position, transform.rotation).GetComponent<CommonBullet>();
+        CommonBullet currentBullet = Instantiate(bullet, shootPoint.position, transform.rotation).GetComponent<CommonBullet>();
 
         currentBullet.damage = damage;
         currentBullet.speed = bulletSpeed;
